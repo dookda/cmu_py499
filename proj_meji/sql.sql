f76211c0d10a55f854409dfc38336369944e2ffe
dookda <sakda.homhuan@gmail.com>
2024-05-12 21:32:57 +0700

    update

--- proj_meji/sql.sql
+++ proj_meji/sql.sql
@@ -1,22 +1,23 @@
 -- 1. create building line segment
 CREATE TABLE cm_bb AS
-WITH boundary_points AS (
+WITH boundaries AS (
+  SELECT id, ST_Boundary(ST_OrientedEnvelope(geom)) AS boundary
+  FROM public.cm_building_4326
+),
+points AS (
   SELECT id, (dp).path[1] AS pt_order, (dp).geom AS pt
-  FROM public.cm_building_orbb,
-  LATERAL ST_DumpPoints(ST_Boundary(geom)) AS dp
+  FROM boundaries,
+  LATERAL ST_DumpPoints(boundary) AS dp
 )
-, line_segments AS (
-  SELECT bp1.id, ST_MakeLine(bp1.pt, bp2.pt) AS geom
-  FROM boundary_points bp1
-  JOIN boundary_points bp2 ON bp1.id = bp2.id AND bp1.pt_order + 1 = bp2.pt_order
-)
-SELECT * FROM line_segments;
+SELECT p1.id, ST_MakeLine(p1.pt, p2.pt) AS segment
+FROM points p1
+JOIN points p2 ON p1.id = p2.id AND p1.pt_order = p2.pt_order - 1;
 
 -- 2. add gid column with serial
 ALTER TABLE cm_bb ADD COLUMN gid SERIAL;
 CREATE INDEX idx_cm_bb_geom ON public.cm_bb USING GIST(geom);
 
--- 3. ใช้ 
+-- 3. ใช้ Join attributes by nearest
 WITH near AS (
 	SELECT MIN(distance), gid3
 	FROM public.bb_near_road
