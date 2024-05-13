44a428ceebf5bb87ff783fb0373ac09f34189c5f
dookda <sakda.homhuan@gmail.com>
2024-05-13 09:36:21 +0700

    update sql for meji

--- proj_meji/sql.sql
+++ proj_meji/sql.sql
@@ -17,11 +17,12 @@ JOIN points p2 ON p1.id = p2.id AND p1.pt_order = p2.pt_order - 1;
 ALTER TABLE cm_bb ADD COLUMN gid SERIAL;
 CREATE INDEX idx_cm_bb_geom ON public.cm_bb USING GIST(geom);
 
--- 3. ใช้ Join attributes by nearest
-WITH near AS (
-	SELECT MIN(distance), gid3
-	FROM public.bb_near_road
-	GROUP BY id, gid3)
-SELECT * FROM near n
-JOIN bb b ON b.gid3 = n.gid3
+-- 3. create centroid of idx_cm_bb_geom
+-- 4. ใช้ Join attributes by nearest ใน QGIS ในการเชื่อมข้อมูลจากตาราง cm_building_4326 และ cm_bb โดยใช้คอลัมน์ id และ gid ตามลำดับ
+-- 5. join ข้อมูลจากข้อ 4 กับตาราง cm_bb โดยใช้คอลัมน์ gid 
+-- 4. สร้างตาราง cm_bb_near_road จากการ Join ในข้อ 3
+CREATE TABLE cm_bd_length AS
+SELECT DISTINCT ON (id)
+  id, distance, geom
+FROM public.cm_bb_near_road;
 
